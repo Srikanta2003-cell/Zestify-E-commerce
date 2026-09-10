30e517d7d5a4f381357b478c86fe8bfea6b2c5d0
Srikanta Kumar Sahu <sahusrikant588@gmail.com>
2026-09-10 20:35:02 +0530

    Create ZestifyApplication.java

---
+++ Zestify/ZestifyApplication.java
@@ -0,0 +1,19 @@
+```java
+package com.zestify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZestifyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+            ZestifyApplication.class,
+            args
+        );
+
+    }
+}
+```
